ebce53a594a16b7715087ed49c1536ecae055330
Prakruti Pathak <prakrutipathak10@gmail.com>
2023-06-24 13:57:37 +0530

    modified code

--- crop.py
+++ crop.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 
